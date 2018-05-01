6941c83c9473501e7ecbea9dec88c786c71e2da2
Ilia Kebets <kebetsi@gmail.com>
2018-05-01 11:05:58 +0200

    add style to table

--- src/App.vue
+++ src/App.vue
@@ -87,4 +87,28 @@ function inspect(item) {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+table {
+    border: 2px solid #42b983;
+    border-radius: 3px;
+    background-color: #fff;
+}
+
+th, td {
+    min-width: 120px;
+    padding: 10px 20px;
+}
+
+td {
+    background-color: #f9f9f9;
+}
+
+th {
+    background-color: #f0f0f0;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
 </style>
