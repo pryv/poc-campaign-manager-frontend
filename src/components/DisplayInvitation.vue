599ab21024d46556a4d25f2de5f89704047b1a19
Ilia Kebets <kebetsi@gmail.com>
2018-05-09 14:00:50 +0200

    create hardcoded DisplayInvitation page

---
+++ src/components/DisplayInvitation.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="DisplayInvitation">
+        <h2>Invitation to campaign</h2>
+        <br>
+        Title: {{ campaign.title }}<br>
+        Description: {{ campaign.description }}<br>
+        Permissions: {{ campaign.permissions }}<br>
+        Pryv App Id: {{ campaign.pryvAppId }}<br>
+        Created: {{ createdReadable }}<br>
+        Requester: {{ requester.username }}<br>
+
+        <button>Pryv Sign in button</button>
+    </div>
+</template>
+
+<script>
+  import Invitations from '@/models/invitations';
+
+  export default {
+    name: 'DisplayInvitation',
+    data () {
+      return {
+        invitationsModel: new Invitations({
+          username: this.$route.query.username || 'bob',
+          token: 'TODO'
+        }),
+        requester: {
+          username: 'empty'
+        },
+        campaign: {
+          id: 'c1n3ok12o3kn12ok3',
+          title: 'Blood Pressure',
+          description: 'The goal of this campaign is to review the blood pressure of patients aged 18-52 to improve their well-being.' +
+          '\n\n' +
+          'Study made by John Hopkins University',
+          created: (Date.now() / 1000),
+          permissions: 'permissions array'
+        }
+      }
+    },
+    computed: {
+      createdReadable() {
+        return new Date(this.created * 1000).toString();
+      }
+    },
+    created() {
+      this.requester.username = this.$route.query.requester;
+      this.campaign.id = this.$route.query.campaignId;
+    },
+    methods: {
+      back() {
+        this.$router.replace('/account/' + this.user.username);
+      },
+      async create() {
+
+      }
+    }
+  };
+</script>
+
+<!-- styling for the component -->
+<style>
+    #DisplayInvitation {
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import CampaignList from '@/components/CampaignList';
 import CreateCampaign from '@/components/CreateCampaign';
 import CreateInvitation from '@/components/CreateInvitation';
+import DisplayInvitation from '@/components/DisplayInvitation';
 import Hello from '@/components/HelloWorld';
 
 Vue.use(Router);
@@ -23,6 +24,11 @@ export default new Router({
       path: '/invitations/new',
       name: 'CreateInvitation',
       component: CreateInvitation
+    },
+    {
+      path: '/invitations/view',
+      name: 'DisplayInvitation',
+      component: DisplayInvitation
     },
     {
       path: '/hello/',
