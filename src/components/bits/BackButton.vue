084e4f44f13f3da7da23aef4f3da720d8afda2b5
Ilia Kebets <kebetsi@gmail.com>
2018-07-09 16:42:15 +0200

    make back button text configurable

--- src/components/bits/BackButton.vue
+++ src/components/bits/BackButton.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="BackButton">
-    <button @click="back">Back</button>
+    <button @click="back">{{ text }}</button>
   </div>
 </template>
 
 <script>
   export default {
     name: 'BackButton',
+    props: [
+      'buttonText'
+    ],
     methods: {
       back() {
         this.$router.back();
+      }
+    },
+    computed: {
+      text() {
+        if (this.buttonText == null) {
+          return 'Back';
+        } else {
+          return this.buttonText;
+        }
       }
     }
   }
