86e5554367b74f1b5c878810e3d667c4934624d0
Ilia Kebets <kebetsi@gmail.com>
2018-07-11 17:28:05 +0200

    vuetify view campaign

--- src/components/bits/Campaign.vue
+++ src/components/bits/Campaign.vue
@@ -1,32 +1,54 @@
 <template>
   <div class="Campaign">
 
-    <table>
-      <tr v-if="hasRequester">
-        <td>Requester:</td>
-        <td>{{ campaign.requester }}</td>
-      </tr>
-      <tr>
-        <td>Title:</td>
-        <td>{{ campaign.title }}</td>
-      </tr>
-      <tr>
-        <td>Description:</td>
-        <td>{{ campaign.description }}</td>
-      </tr>
-      <tr>
-        <td>Permissions:</td>
-        <td>{{ campaign.permissions }}</td>
-      </tr>
-      <tr>
-        <td>Pryv App Id:</td>
-        <td>{{ campaign.pryvAppId }}</td>
-      </tr>
-      <tr>
-        <td>Created:</td>
-        <td>{{ createdReadable }}</td>
-      </tr>
-    </table>
+    <v-layout row v-if="hasRequester">
+      <v-flex xs4>
+        <v-subheader>Requester:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ campaign.requester }}</v-subheader>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Title:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ campaign.title }}</v-subheader>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Description:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ campaign.description }}</v-subheader>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Permissions:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ campaign.permissions }}</v-subheader>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Pryv AppId:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ campaign.pryvAppId }}</v-subheader>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Created:</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-subheader>{{ createdReadable }}</v-subheader>
+      </v-flex>
+    </v-layout>
 
   </div>
 </template>
@@ -39,12 +61,24 @@
     ],
     computed: {
       createdReadable() {
-        return new Date(this.campaign.created * 1000).toString();
+        return printDate(this.campaign.created);
       },
       hasRequester() {
         return (this.campaign.requester != null);
       }
     }
+  }
+
+  function printDate(timestamp) {
+    function pad2(n) { return n < 10 ? '0' + n : n }
+
+    const date = new Date(timestamp * 1000);
+
+    return pad2(date.getHours()) + ':' +
+      pad2(date.getMinutes()) + ' ' +
+      pad2( date.getDate()) + '/' +
+      pad2(date.getMonth() + 1) + '/' +
+      date.getFullYear().toString();
   }
 </script>
 
